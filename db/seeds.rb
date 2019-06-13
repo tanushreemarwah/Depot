c2d20643418f2460116acad3c2929c02ff872e08
Tanushree Marwah <tanushreemarwah@Tanushrees-MacBook-Pro.local>
2019-06-13 10:58:28 +0530

    Create, edit, delete Products

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(
+    title: 'Seven Mobile apps in seven weeks',
+    description: %{
+        <p>
+        <em>Native Apps, Multiple Platforms</em>
+        Answer the question "Can we build this for ALL the devices?" with 
+        all resounding Yes.This book will help you get there with a 
+        real world introduction to seven platforms whether youre 
+        new to mobile or an experienced developer needing to expand 
+        your options. Plus youll find out which cross-platform 
+        solution makes the most snese for your needs.
+        </p>
+    },
+    image_url: '7apps.jpg',
+    price: 62.00
+)
