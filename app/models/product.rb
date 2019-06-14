018f6aaa235a96af3f860cf4a041b496c69f3e72
Tanushree Marwah <tanushreemarwah@Tanushrees-MacBook-Pro.local>
2019-06-14 12:43:51 +0530

    Add product to Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
 
     validates :title, uniqueness: true, length: { minimum: 10 }
     validates :title, :description, :image_url, presence: true
@@ -7,4 +9,12 @@ class Product < ApplicationRecord
         message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
     }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    private
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.emoty?
+                errors.add(:base, 'Line Items Present')
+                throw :abort
+            end
+        end
 end
